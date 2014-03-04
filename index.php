328607fa2ca467e8d939abe2c63f04d2eed9e51e
Jordie Puchinger <jordie@collabers.com>
2014-03-04 14:01:21 -0700

    commit

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
-<?php
-echo "hello world";
-?>
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+
+<body>
+
+    <div class="iframe-canvas">
+        <iframe src="http://bit.ly/SBappDoc"></iframe>
+    </div>
+
+    <script type="text/javascript" language="javascript">
+        $('.iframe-canvas').css('height', $(window).height() + 70 +'px');
+    </script>
+</body>
+</html>
